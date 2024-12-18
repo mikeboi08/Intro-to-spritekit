229dcf02d634f8b5b24b2fd2ab680007301d4cd0
Micah Howard <micahhoward99@gmail.com>
2024-12-18 09:14:00 -0500

    inital commit

---
+++ actual game/GameScene.swift
@@ -0,0 +1,25 @@
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene,SKPhysicsContactDelegate {
+    var ball:SKSpriteNode!
+    var paddle:SKSpriteNode!
+    
+    override func didMove(to view: SKView) {
+      
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+       
+    }
+    
+}
---
+++ actual game/actual gameTests/actual_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  actual_gameTests.swift
+//  actual gameTests
+//
+//  Created by Micah Howard on 12/15/24.
+//
+
+import Testing
+@testable import actual_game
+
+struct actual_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
